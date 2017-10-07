e3cef5ed3e369d8d9f63325d9765dcf072c70827
amigosdovaz <amigosdovaz@gmail.com>
2017-10-07 17:50:12 -0300

    Primeira experiência com o GitHub

---
+++ Empresa.Dominio/Contato.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Empresa.Dominio
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+        public string Assunto { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
---
+++ Empresa.Mvc/Controllers/ContatosController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Empresa.Repositorios.SqlServer;
+using Empresa.Dominio;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Empresa.Mvc.Controllers
+{
+    public class ContatosController : Controller
+    {
+        private readonly EmpresaDbContext _contexto;
+        public ContatosController(EmpresaDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(_contexto.Contatos.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contato contato)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(contato);
+            }
+
+            _contexto.Add(contato);
+            _contexto.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
